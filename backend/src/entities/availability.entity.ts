97ddc49f4c008a4237f4e0f495ac0b756d8a4516
wozprzemek <wozprzemek@gmail.com>
2023-02-19 10:08:44 +0100

    MikroORM template update

--- backend/src/entities/availability.entity.ts
+++ backend/src/entities/availability.entity.ts
@@ -1,3 +1,7 @@
-export interface Availability {
-    
+import { Entity, PrimaryKey } from '@mikro-orm/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity()
+export class Availability {
+
 }
---
+++ backend/src/entities/meeting.entity.ts
@@ -0,0 +1,7 @@
+import { Entity, PrimaryKey } from '@mikro-orm/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity()
+export class Meeting {
+
+}
